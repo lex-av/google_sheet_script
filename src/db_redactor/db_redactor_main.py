a60d7fca897db16b60d326590fe250161dfce864
alex <allexander0509@gmail.com>
2022-07-07 02:49:03 +0300

    Refactor of the main loop in db_redactor: db updates moved to independent functions for better codebase

--- src/db_redactor/db_redactor_main.py
+++ src/db_redactor/db_redactor_main.py
@@ -28,6 +28,75 @@ def google_data_row_parse(row: List, rub_rate: float):
     return order_id, price_usd, price_rub, delivery_time
 
 
+def update_remote_table(db_session, table_data, rub_rate):
+    """
+    Inserts table data from google-table into secondary
+    remote orders table with deletion of older records
+
+    :param db_session: db_session: session obj from sqlalchemy.orm.sessionmaker
+    :param table_data: list of rows from google API
+    :param rub_rate: up to date rub rate
+    """
+
+    db_session.query(RemoteOrders).delete()
+    db_session.commit()
+    for prime_id, row in enumerate(table_data, start=1):
+        order_id, price_usd, price_rub, delivery_time = google_data_row_parse(row, rub_rate)
+        remote_row = RemoteOrders(
+            id=prime_id,
+            order_id=order_id,
+            price_usd=price_usd,
+            price_rub=price_rub,
+            delivery_time=delivery_time,
+        )
+        db_session.add(remote_row)
+    db_session.commit()
+
+
+def update_local_table_with_new_rows(db_session, table_data, rub_rate):
+    """
+    Adds only new rows to local orders table from table, fetched from google
+
+    :param db_session: db_session: session obj from sqlalchemy.orm.sessionmaker
+    :param table_data: list of rows from google API
+    :param rub_rate: up to date rub rate
+    """
+    for row in table_data:
+        order_id, price_usd, price_rub, delivery_time = google_data_row_parse(row, rub_rate)
+        row_exists = db_session.query(LocalOrders.order_id).filter_by(order_id=order_id).first() is not None
+
+        if not row_exists:
+            local_row = LocalOrders(
+                order_id=order_id,
+                price_usd=price_usd,
+                price_rub=price_rub,
+                delivery_time=delivery_time,
+            )
+            db_session.add(local_row)
+    db_session.commit()
+
+
+def clear_local_table(db_session):
+    """
+    Deletes rows from local orders teble by order_id. If it is not
+    present in remote table, then it was deleted from original google table.
+
+    :param db_session: db_session: session obj from sqlalchemy.orm.sessionmaker
+    :param table_data: list of rows from google API
+    :param rub_rate: up to date rub rate
+    """
+
+    for current_order_id in db_session.query(LocalOrders.order_id):
+        current_order_id = current_order_id[0]
+        row_missing_remote = (
+            db_session.query(RemoteOrders.order_id).filter_by(order_id=current_order_id).first() is None
+        )
+
+        if row_missing_remote:
+            db_session.query(LocalOrders).filter(LocalOrders.order_id == current_order_id).delete()
+            db_session.commit()
+
+
 def main():
     """
     Main loop of db_redactor. Updates main table every n seconds, using
@@ -53,47 +122,15 @@ def main():
         google_data.pop(0)  # Skip table header
 
         # Updating secondary remote orders table with old data removal
-        session.query(RemoteOrders).delete()
-        session.commit()
-        for prime_id, row in enumerate(google_data, start=1):
-            order_id, price_usd, price_rub, delivery_time = google_data_row_parse(row, rub_rate)
-            remote_row = RemoteOrders(
-                id=prime_id,
-                order_id=order_id,
-                price_usd=price_usd,
-                price_rub=price_rub,
-                delivery_time=delivery_time,
-            )
-            session.add(remote_row)
-        session.commit()
+        update_remote_table(session, google_data, rub_rate)
         print("Updated remote")
 
         # Adding only new entries to local orders table
-        for row in google_data:
-            order_id, price_usd, price_rub, delivery_time = google_data_row_parse(row, rub_rate)
-            row_exists = session.query(LocalOrders.order_id).filter_by(order_id=order_id).first() is not None
-
-            if not row_exists:
-                local_row = LocalOrders(
-                    order_id=order_id,
-                    price_usd=price_usd,
-                    price_rub=price_rub,
-                    delivery_time=delivery_time,
-                )
-                session.add(local_row)
-        session.commit()
-        print("Updated local")
+        update_local_table_with_new_rows(session, google_data, rub_rate)
 
         # Deleting rows from local orders table, that are already not present in google table
-        for current_order_id in session.query(LocalOrders.order_id):
-            current_order_id = current_order_id[0]
-            row_missing_remote = (
-                session.query(RemoteOrders.order_id).filter_by(order_id=current_order_id).first() is None
-            )
-
-            if row_missing_remote:
-                session.query(LocalOrders).filter(LocalOrders.order_id == current_order_id).delete()
-                session.commit()
+        clear_local_table(session)
+        print("Updated local")
 
         time.sleep(2)
 
