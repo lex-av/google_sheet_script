24479df6057f33c47f4682078dcc8114509d38c3
alex <allexander0509@gmail.com>
2022-07-07 01:27:02 +0300

    db_redactor_main.py - added remote_table refreshing

--- src/db_redactor/db_redactor_main.py
+++ src/db_redactor/db_redactor_main.py
@@ -1,4 +1,6 @@
 import datetime
+import time
+from typing import List
 
 from api_clients.api_client_bank import check_currency_rate
 from api_clients.api_google import get_spreadsheet_table
@@ -7,7 +9,23 @@ from db.base import DeclarativeBase, engine
 from db.db_tables import RemoteOrders
 from sqlalchemy.orm import sessionmaker
 
-# import time
+
+def google_data_row_parse(row: List, rub_rate: float):
+    """
+    Converts list of given values to tuple of
+    types, suitable for database
+
+    :param row: list of values
+    :param rub_rate: up to date rub_rate
+    :return:
+    """
+
+    order_id = (row[1],)
+    price_usd = (row[2],)
+    price_rub = (float(row[2]) * rub_rate,)
+    delivery_time = datetime.datetime.strptime(row[3], "%d.%m.%Y")
+
+    return order_id, price_usd, price_rub, delivery_time
 
 
 def main():
@@ -26,19 +44,23 @@ def main():
         file_str = f.readline()
     rub_rate = float(file_str.split("|")[0])
 
-    # while True:
-    # time_start = time.time()
-    google_data = get_spreadsheet_table()
+    while True:
+        google_data = get_spreadsheet_table()
+        google_data.pop(0)
+        session.query(RemoteOrders).delete()
 
-    session.query(RemoteOrders).delete()
-    new_line = RemoteOrders(
-        order_id=google_data[1][1],
-        price_usd=google_data[1][2],
-        price_rub=float(google_data[1][2]) * rub_rate,
-        delivery_time=datetime.datetime.strptime(google_data[1][3], "%d.%m.%Y"),
-    )
-    session.add(new_line)
-    session.commit()
+        for row in google_data:
+            order_id, price_usd, price_rub, delivery_time = google_data_row_parse(row, rub_rate)
+            new_line = RemoteOrders(
+                order_id=order_id,
+                price_usd=price_usd,
+                price_rub=price_rub,
+                delivery_time=delivery_time,
+            )
+            session.add(new_line)
+        session.commit()
+        print("Updated DB")
+        time.sleep(2)
 
 
 if __name__ == "__main__":
